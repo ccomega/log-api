d3938710b6eb7579f5e1ddb84564a9e934ca7bfe
RubenHetKonijn <64010592+RubenHetKonijn@users.noreply.github.com>
2020-08-22 12:39:17 +0200

    Create log.lua

---
+++ log.lua
@@ -0,0 +1,20 @@
+function log(type,logItem)
+  oldTextColor = term.getTextColor()
+  if type == "warn" then
+    term.setTextColor(colors.orange)
+    print("[WARN] ".. logItem)
+    term.setTextColor(oldTextColor)
+  elseif type == "error" then
+    term.setTextColor(colors.red)
+    print("[ERROR] ".. logItem)
+    term.setTextColor(oldTextColor)
+  elseif type == "debug" then
+    term.setTextColor(colors.green)
+    print("[DEBUG] ".. logItem)
+    term.setTextColor(oldTextColor)
+  else 
+    print("["..type.."] ".. logItem)
+  end
+end
+ 
+return log
