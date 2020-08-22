8c76675c130b9afcd44e601579317bd93d573ee8
RubenHetKonijn <64010592+RubenHetKonijn@users.noreply.github.com>
2020-08-22 12:45:32 +0200

    Create example.lua

---
+++ example.lua
@@ -0,0 +1,8 @@
+log = require("log")
+
+print("This is a print command")
+
+log("warn","This is a warn")
+log("error","This is an error")
+log("debug","This is debug info")
+log("TEST","This is a custom log")
